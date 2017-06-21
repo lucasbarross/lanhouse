04420a80923bdc0b3de3032313215ba58f06de5d
sgfl <sgfl@cin.ufpe.br>
2017-06-21 14:46:27 -0300

    Lendo arquivo no main

--- src/br/ufpe/cin/lanhouse/fachada/Administrador.java
+++ src/br/ufpe/cin/lanhouse/fachada/Administrador.java
@@ -1,6 +1,6 @@
 package br.ufpe.cin.lanhouse.fachada;
-import br.ufpe.cin.lanhouse.basicas.Aplicativo;
-import br.ufpe.cin.lanhouse.exceptions.AppNaoEncontradoException;
+import br.ufpe.cin.lanhouse.basicas.*;
+import br.ufpe.cin.lanhouse.exceptions.*;
 import br.ufpe.cin.lanhouse.negocios.*;
 
 import java.io.BufferedReader;
@@ -14,23 +14,20 @@ public class Administrador {
     private CadastroImpressoras cadastroImpressoras;
     private CadastroPessoas cadastroPessoas;
 
-    public void lerConfig() throws IOException {
-        FileReader entrada = new FileReader("config.txt");
-        BufferedReader br = new BufferedReader(entrada);
-        String linha = br.readLine();
-        boolean array;
-
-        array = linha.equals("array");
-
-        cadastroAplicativos= new CadastroAplicativos(array);
-        cadastroComputadores = new CadastroComputadores(array);
-        cadastroImpressoras = new CadastroImpressoras(array);
-        cadastroPessoas = new CadastroPessoas(array);
+    public Administrador(CadastroAplicativos cadastroAplicativos, CadastroComputadores cadastroComputadores, CadastroImpressoras cadastroImpressoras, CadastroPessoas cadastroPessoas) {
+        this.cadastroAplicativos = cadastroAplicativos;
+        this.cadastroComputadores = cadastroComputadores;
+        this.cadastroImpressoras = cadastroImpressoras;
+        this.cadastroPessoas = cadastroPessoas;
     }
 
     public Aplicativo procurarApp(String nome) throws AppNaoEncontradoException {
         return cadastroAplicativos.procurar(nome);
     }
 
+    public String usarComputador(String cpf) throws PessoaNaoEncontradaException, SemComputadorException {
+        Pessoa usuario = cadastroPessoas.procurar(cpf);
+        return usuario.usarComputador();
+    }
 
 }
--- src/br/ufpe/cin/lanhouse/programa/LanHouse.java
+++ src/br/ufpe/cin/lanhouse/programa/LanHouse.java
@@ -1,18 +1,30 @@
 package br.ufpe.cin.lanhouse.programa;
 
-import br.ufpe.cin.lanhouse.basicas.Aplicativo;
-import br.ufpe.cin.lanhouse.interfaces.RepositorioAplicativos;
-import br.ufpe.cin.lanhouse.repositorios.RepositorioListaAplicativos;
+import br.ufpe.cin.lanhouse.basicas.*;
+import br.ufpe.cin.lanhouse.fachada.Administrador;
+import br.ufpe.cin.lanhouse.negocios.*;
+import br.ufpe.cin.lanhouse.repositorios.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LanHouse {
 
 	public static void main(String[] args) {
-		RepositorioListaAplicativos apps = new RepositorioListaAplicativos();
-		apps.inserir(new Aplicativo("Skype", 10, 100));
-		apps.inserir(new Aplicativo("Skype2", 10, 100));
-		apps.inserir(new Aplicativo("Skype3", 10, 100));
-		apps.inserir(new Aplicativo("Skype4", 10, 100));
-		System.out.println(apps.getApps());
+		try {
+			FileReader entrada = null;
+			entrada = new FileReader("config.txt");
+			BufferedReader br = new BufferedReader(entrada);
+			String linha = null;
+			linha = br.readLine();
+			boolean array;
+			array = linha.equals("array");
+			Administrador adm = new Administrador(new CadastroAplicativos(array), new CadastroComputadores(array), new CadastroImpressoras(array), new CadastroPessoas(array));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
