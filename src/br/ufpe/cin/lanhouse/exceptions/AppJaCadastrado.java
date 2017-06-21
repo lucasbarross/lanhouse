d031a2dac981c7a7ae42e4b678ac526c2c90d2c4
vlma <vlma@cin.ufpe.br>
2017-06-21 16:02:32 -0300

    +Exceptions +main

--- src/br/ufpe/cin/lanhouse/exceptions/AppJaCadastrado.java
+++ src/br/ufpe/cin/lanhouse/exceptions/AppJaCadastrado.java
@@ -3,5 +3,8 @@ package br.ufpe.cin.lanhouse.exceptions;
 /**
  * Created by vlma on 21/06/2017.
  */
-public class AppJaCadastrado {
+public class AppJaCadastrado extends Exception {
+    public AppJaCadastrado() {
+        super("App já cadastrado.");
+    }
 }
--- src/br/ufpe/cin/lanhouse/exceptions/PessoaJaCadastradaException.java
+++ src/br/ufpe/cin/lanhouse/exceptions/PessoaJaCadastradaException.java
@@ -3,5 +3,8 @@ package br.ufpe.cin.lanhouse.exceptions;
 /**
  * Created by vlma on 21/06/2017.
  */
-public class PessoaJaCadastradaException {
+public class PessoaJaCadastradaException extends Exception {
+    public PessoaJaCadastradaException() {
+        super("Pessoa já está cadastrada.");
+    }
 }
